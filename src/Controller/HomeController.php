2d596d6d8a75c5fddc1bbfa593723d9da4812308
ChristopherSaury <saurychristopher@gmail.com>
2021-12-11 15:06:49 +0100

    cgu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,6 +13,13 @@ class HomeController extends AbstractController
     public function homePage(): Response
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/cgu', name: 'cgu')]
+    public function cgu(){
+        
+        return $this->render('home/cgu.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
